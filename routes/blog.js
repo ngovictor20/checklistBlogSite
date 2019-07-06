0f4e3ead5db6e69413006ed232bf152c54846ce6
Victor <ngo.victor20@gmail.com>
2019-07-06 00:40:01 -0400

    Updating all with multer, file checking with blog registration and editing

--- routes/blog.js
+++ routes/blog.js
@@ -4,9 +4,11 @@ var bodyParser = require('body-parser');
 var mongoose = require("mongoose");
 var User = require("../models/user");
 var Blog = require("../models/blog")
+var Image = require("../models/image")
 var passport = require("passport")
 var middleware = require("./middleware")
 var multer = require("multer")
+var upload = multer()
 
 //show all blogs
 router.get("/blog", middleware.isLoggedIn, function (req, res) {
@@ -27,10 +29,11 @@ router.get("/blog/new", middleware.isLoggedIn, function (req, res) {
 //specific blog
 router.get("/blog/:blog_id", function (req, res) {
     console.log(req.params.blog_id)
-    Blog.findById(req.params.blog_id, function (err, foundBlog) {
+    Blog.findById(req.params.blog_id).populate("image").exec(function (err, foundBlog) {
         if (err) {
             console.log(err)
         } else {
+            console.log(foundBlog)
             res.render("showBlogSpecific", { blog: foundBlog })
         }
     })
@@ -42,7 +45,7 @@ router.post("/blog", middleware.isLoggedIn, upload.single('fileupload'), functio
     console.log(req.body.blog)
     console.log(req.user)
     console.log(req.file)
-    if (req.file) {
+    if (req.file && req.file.buffer.byteLength() != 0) {
         //res.json({file:req.file});
         var imgBody = {
             fileName: req.file.originalname,
@@ -57,16 +60,16 @@ router.post("/blog", middleware.isLoggedIn, upload.single('fileupload'), functio
                 console.log(err)
             } else {
                 console.log("======Created Image======")
-                console.log(newImage.data)
+                //console.log(newImage.data)
                 //res.redirect("/image")
+                blogBody.image = newImage
                 Blog.create(blogBody, function (err, newBlog) {
                     if (err) {
                         console.log(err)
                     }
                     else {
                         console.log("======Created Blog======")
-                        console.log(newBlog)
-                        newBlog.image = newImage;
+                        //console.log(newBlog)
                         res.redirect("/blog/"+newBlog._id)
                     }
                 })
@@ -88,14 +91,31 @@ router.get("/blog/:blog_id/edit", middleware.checkBlogOwnership, function (req, 
 })
 
 //update
-router.put("/blog/:blog_id", middleware.checkBlogOwnership, function (req, res) {
-    Blog.findByIdAndUpdate(req.params.blog_id, req.body.blog, function (err, newBlog) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(newBlog)
-            res.redirect("/blog/" + req.params.blog_id)
+router.put("/blog/:blog_id", middleware.checkBlogOwnership, upload.single("fileupload"), function (req, res) {
+    let imgBody
+    Blog.findById(req.params.blog_id).populate("image").exec(function(err,newBlog){
+        if(req.file && Buffer.compare(req.file.buffer,newBlog.image.data) != 0){
+            imgBody = {
+                fileName: req.file.originalname,
+                encoding: req.file.encoding,
+                contentType: req.file.mimetype,
+                data: req.file.buffer
+            }
+            if(req.file.buffer.byteLength() != 0){
+                Image.create(imgBody,function(err,newImage){
+                    if(err){
+                        console.log("something went wrong")
+                    }else{
+                        console.log("created new image")
+                        newBlog.image = newImage
+                    }
+                })
+            }
+        }else{
+            newBlog.title = req.body.blog.title
+            newBlog.text = req.body.blog.text
         }
+        newBlog.save()
     })
 })
 
